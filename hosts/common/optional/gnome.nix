6733e6d48a4eeba76858dc8333f06bc59985225a
Thomas Stachl <i@t5.st>
2022-10-12 22:28:47 -0700

    gnome cleanup

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -3,20 +3,34 @@
   services = {
     xserver = {
       enable = true;
+
+      # configure keymap in X11
+      layout = "us";
+
       desktopManager.gnome = {
         enable = true;
       };
+
       displayManager.gdm = {
         enable = true;
         autoSuspend = false;
       };
     };
+
     geoclue2.enable = true;
     gnome.games.enable = false;
+
+    # system tray icons
+    udev.packages = with pkgs.gnome;
+      [ gnome-settings-daemon ];
+    
+    # gnome crypto services and tools package
+    dbus.packages = [ pkgs.gnome.dconf pkgs.gcr ];
+    
+    avahi.enable = false;
   };
 
   # Fix broken stuff
-  services.avahi.enable = false;
   networking.networkmanager.enable = false;
 
   # Remove bloat from Gnome
@@ -38,14 +52,4 @@
     hitori # sudoku game
     atomix # puzzle game
   ]);
-
-  # system tray icons
-  services.udev.packages = with pkgs.gnome;
-    [ gnome-settings-daemon ];
-  
-  # gnome crypto services and tools package
-  services.dbus.packages = [ pkgs.gcr ];
-
-  # configure keymap in X11
-  services.xserver.layout = "us";
 }
