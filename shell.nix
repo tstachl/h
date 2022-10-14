afbff33ed76cce73b318822a38c11212ddc2dd91
Thomas Stachl <i@t5.st>
2022-10-14 12:54:05 -0700

    added shellHook to nix shell

--- shell.nix
+++ shell.nix
@@ -12,13 +12,33 @@
 }: pkgs.mkShell {
   NIX_CONFIG = "experimental-features = nix-command flakes repl-flake";
 
+  shellHook = ''
+    gpg --receive-keys DE749C31D060A160
+    echo "$( \
+      gpg --list-keys \
+      | grep DE749C31D060A160 -A 1 \
+      | head -1 | tr -d '[:space:]' \
+    ):6:" | gpg --import-ownertrust;
+
+    echo "pinentry-program $(which pinentry)" > ~/.gnupg/gpg-agent.conf
+
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+    export PINENTRY_USER_DATA="USE_CURSES=1"
+    export GPG_TTY=$(tty)
+
+    gpg-connect-agent reloadagent /bye
+    gpg-connect-agent updatestartuptty /bye
+  '';
+
   nativeBuildInputs = with pkgs; [
     nix
     home-manager
     git
+    git-crypt
 
     sops
     gnupg
+    pinentry
     age
   ];
 }
