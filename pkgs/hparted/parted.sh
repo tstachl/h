e5f0c687e76855eb4cb0d2fdfec7396e1d60b2f9
Thomas Stachl <i@t5.st>
2022-10-15 12:55:40 -0700

    fixed all shell issues

--- pkgs/hparted/parted.sh
+++ pkgs/hparted/parted.sh
@@ -8,7 +8,7 @@ fi
 
 function usage {
   cat <<EOM
-Usage: $(basename $0) [options] <device> <hostname>
+Usage: $(basename "$0") [options] <device> <hostname>
 
 Options:
   -c          create partitions
@@ -23,37 +23,48 @@ EOM
 }
 
 create=false
-encrypt=false
+#encrypt=false
 mount=false
 
 while getopts ':ecmh' opt; do
   case $opt in
-    e) encrypt=true;;
     c) create=true;;
+#    e) encrypt=true;;
     m) mount=true;;
     h) usage;;
+    *) ;;
   esac
 done
 
-device=${@:$OPTIND:1}
-hostname=${@:$OPTIND+1:1}
+device=$( printf '%s\n' "${@:$OPTIND:1}" )
+hostname=$( printf '%s\n' "${@:$OPTIND+1:1}" )
 
-if [ -z $device ]; then
+if [ -z "$device" ]; then
   echo "Error: device is required (eg. /dev/sda)"
   exit 1
 fi
 
-if [ -z $hostname ]; then
+if [ -z "$hostname" ]; then
   echo "Error: hostname is required (eg. throwaway)"
   exit 1
 fi
 
+# cat << EOM
+# 
+# create=$create
+# encrypt=$encrypt
+# mount=$mount
+# device=$device
+# hostname=$hostname
+# 
+# EOM
+
 if [[ "$create" = true ]]; then
   # Creating Partitions
-  parted $device -- mklabel gpt
-  parted $device -- mkpart primary 512mb 100%
-  parted $device -- mkpart ESP fat32 1mb 512mb
-  parted $device -- set 2 esp on
+  parted "$device" -- mklabel gpt
+  parted "$device" -- mkpart primary 512mb 100%
+  parted "$device" -- mkpart ESP fat32 1mb 512mb
+  parted "$device" -- set 2 esp on
 
   # # Set up encryption
   # #cryptsetup --verify-passphrase -v luksFormat --label=${hostname}_crypt "${device}1"
@@ -61,7 +72,7 @@ if [[ "$create" = true ]]; then
 
   # Formatting Partitions
   #mkfs.btrfs -fL $2 /dev/mapper/$2
-  mkfs.btrfs -fL $hostname "${device}1"
+  mkfs.btrfs -fL "$hostname" "${device}1"
   mkfs.fat -F 32 -n boot "${device}2"
 
   # ln -s "${device}1" /dev/mapper/$hostname
