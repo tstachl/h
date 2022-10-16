167b03ddf77c6b9997f08826db2f505c08262551
Thomas Stachl <i@t5.st>
2022-10-15 22:31:31 -0700

    fix the LC_TIME issue

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -3,7 +3,7 @@
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_TIME = lib.mkDefault "de_AT.UTF-8";
+      LC_TIME = lib.mkDefault "en_US.UTF-8";
     };
     supportedLocales = lib.mkDefault [
       "en_US.UTF-8/UTF-8"
