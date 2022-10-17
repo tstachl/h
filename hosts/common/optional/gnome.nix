3576e8e6871987f225c4e6459d0b852c60a58847
Thomas Stachl <i@t5.st>
2022-10-17 15:23:29 -0700

    fixes #14 and removes some more bloat from gnome

--- hosts/common/optional/gnome.nix
+++ hosts/common/optional/gnome.nix
@@ -29,15 +29,26 @@
 
   # Remove bloat from Gnome
   environment.gnome.excludePackages = (with pkgs; [
+    gnome-console
     gnome-photos
+    gnome-text-editor
     gnome-tour
+    gnome-user-docs
+    xterm
   ]) ++ (with pkgs.gnome; [
     cheese # webcam tool
     gnome-music
+    gnome-contacts
+    gnome-calendar
     gnome-terminal
+    gnome-maps
+    gnome-weather
     gedit
+    simple-scan # document scanner
+    yelp # the help viewer in Gnome
     epiphany # web browser
     geary # email client
+    eog # image viewer
     evince # document viewer
     gnome-characters
     totem # video player
