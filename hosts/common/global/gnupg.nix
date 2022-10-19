f872fce016978213caf44207566e77d36d1eae44
Thomas Stachl <i@t5.st>
2022-10-19 11:06:11 -0700

    enable SSH_AUTH_SOCK

--- hosts/common/global/gnupg.nix
+++ hosts/common/global/gnupg.nix
@@ -1,7 +1,14 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    gnupg
-    pinentry
-  ];
+  environment = {
+    systemPackages = with pkgs; [
+      gnupg
+      pinentry
+    ];
+
+    # required to use yubikey for SSH
+    loginShellInit = lib.mkAfter ''
+      export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+    '';
+  };
 }
