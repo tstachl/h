b05d86498ce4d059c98e00052860845f21f11b66
Thomas Stachl <i@t5.st>
2022-10-20 16:50:38 -0700

    some aliases and functions

--- home/thomas/features/cli/fish.nix
+++ home/thomas/features/cli/fish.nix
@@ -1,5 +1,36 @@
+{ lib, ... }:
 {
   programs.fish = {
     enable = true;
+    
+    shellAliases = {
+      g = "git";
+      "..." = "cd ../..";
+      "...." = "cd ../../..";
+      hms = "home-manager switch --flake ~/Documents/tstachl/h#$USER@$(hostname)";
+    };
+
+    interactiveShellInit = lib.mkAfter ''
+      function d
+        set target ~/Documents/$argv[1]
+        echo "Target: $target"
+
+        if not test -d $target
+          set repo (string split "/" $argv[1])
+          echo "Repo: $repo"
+
+          if not test -d ~/Documents/$repo[1]
+            echo "creating directory ..."
+            mkdir ~/Documents/$repo[1]
+          end
+
+          cd ~/Documents/$repo[1]
+          git clone git@github.com:$argv[1]
+        end
+
+        cd $target
+        nvim .
+      end
+    '';
   };
 }
