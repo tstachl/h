8157194bdcfaf62f1dc5f61a3ed34deed1a6a397
Thomas Stachl <i@t5.st>
2022-10-20 22:13:11 +0000

    not needed

--- hosts/common/optional/sops.nix
+++ hosts/common/optional/sops.nix
@@ -3,9 +3,4 @@
   imports = [
     inputs.sops-nix.nixosModules.sops
   ];
-
-  sops.age.sshKeyPaths = [
-    "/persist/etc/ssh/ssh_host_ed25519_key"
-    "/persist/etc/ssh/ssh_host_rsa_key"
-  ];
 }
