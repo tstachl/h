6bc73bb0e4bc3ef30f385913132328f61151e5ed
Thomas Stachl <i@t5.st>
2022-10-20 16:20:59 -0700

    use github token

--- home/thomas/features/cli/github.nix
+++ home/thomas/features/cli/github.nix
@@ -1,5 +1,9 @@
-{ pkgs, ... }:
+{ lib, ... }:
 {
+  programs.fish.interactiveShellInit = lib.mkAfter ''
+    set -gx GH_TOKEN "$(cat /run/secrets/github_cli)"
+  '';
+
   programs.gh = {
     enable = true;
   };
