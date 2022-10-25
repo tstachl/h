d40e4b78721d38ea40133d4388a66cb07c574dd5
Thomas Stachl <i@t5.st>
2022-10-25 09:34:03 -0700

    some information and sketch out of the file system

--- hosts/odin/README.md
+++ hosts/odin/README.md
@@ -6,3 +6,29 @@
 * Backup
 * Git Backup
 
+
+## Filesystem
+
+### /boot/firmware
+
+This is the first partition on the SD card or the usb hard drive. It'll contain
+all the files required for booting from a Raspberry Pi. It'll be mounted with
+the options `nofail` and `noauto` which should make this partition not show up
+in the OS itself.
+
+### /
+
+The root filesystem will be mounted on `tempfs` because it'll be rebuilt on
+every boot. This will allow the raspberry pi to keep the new machine smell.
+
+### /nix
+
+For nix and the nix store, we will use a btrfs subvol. File on here should be
+persistent and changeable.
+
+### /persist
+
+There are certain files that need to be persisted in between boots like the host
+keys of the machine. We'll be using a `nix-impermanence` configuration to store
+the required files/folders on a persistent drive.
+
--- hosts/odin/hardware-configuration.nix
+++ hosts/odin/hardware-configuration.nix
@@ -15,6 +15,44 @@
     loader.timeout = 5;
   };
 
+  fileSystems = {
+    "/boot/firmware" = {
+      device = "/dev/disk/by-label/FIRMWARE";
+      fsType = "vfat";
+      options = [ "nofail" "noauto" ];
+    };
+
+    "/" = {
+      device = "none";
+      fsType = "tmpfs";
+      options = [ "defaults" "size=2G" "mode=755" ];
+    };
+
+    "/nix" = {
+      device = "/dev/disk/by-label/${hostname}";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+    };
+
+    "/persist" = {
+      device = "/dev/disk/by-label/${hostname}";
+      fsType = "btrfs";
+      options = [ "subvol=persist" "compress=zstd" "noatime" ];
+      neededForBoot = true;
+    };
+
+    "/swap" = {
+      device = "/dev/disk/by-label/${hostname}";
+      fsType = "btrfs";
+      options = [ "subvol=swap" "noatime" "nodatacow" ];
+    };
+  };
+
+  swapDevices = [{
+    device = "/swap/swapfile";
+    size = 4096;
+  }];
+
   sdImage = {
     compressImage = false;
     imageBaseName = "odin";
