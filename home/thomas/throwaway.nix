68ef8aafc531fee02a58805c9c31ae6a468de4c5
Thomas Stachl <i@t5.st>
2022-10-24 19:52:52 -0700

    remove persistence from home-manager

--- home/thomas/throwaway.nix
+++ home/thomas/throwaway.nix
@@ -1,26 +1,8 @@
 { inputs, ... }:
 {
   imports = [
-    inputs.impermanence.nixosModules.home-manager.impermanence
     ./global.nix
     ./features/desktop
     ./features/nvim
   ];
-
-  home.persistence."/persist/home/thomas" = {
-    directories = [
-      # must have at least one bind mount
-      "Workspace"
-      {
-        directory = ".config/BraveSoftware/";
-        method = "symlink";
-      }
-    ];
-
-    files = [
-      ".config/monitors.xml"
-    ];
-
-    allowOther = true;
-  };
 }
