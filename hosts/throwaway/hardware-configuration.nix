ef2721c73c2ada4dcb37d43e89f665fb86d86180
Thomas Stachl <i@t5.st>
2022-10-26 11:07:38 -0700

    +fuse

--- hosts/throwaway/hardware-configuration.nix
+++ hosts/throwaway/hardware-configuration.nix
@@ -3,7 +3,7 @@
   imports = [
     (modulesPath + "/profiles/qemu-guest.nix")
     ../common/optional/filesystem.nix
-    # ../common/optional/fuse.nix
+    ../common/optional/fuse.nix
     # ../common/optional/state.nix
     ../common/optional/systemd-boot.nix
   ];
