37c35ad2bc6e6596414673411d9cce16e65f930c
Thomas Stachl <i@t5.st>
2022-10-26 11:16:30 -0700

    +nix.settings

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -11,24 +11,24 @@
       trusted-users = [ "root" "@wheel" ];
       auto-optimise-store = lib.mkDefault true;
     };
-    package = pkgs.nixUnstable;
-    extraOptions = ''
-      auto-optimise-store = true
-      experimental-features = nix-command flakes repl-flake
-      warn-dirty = false
-    '';
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 30d";
-    };
+    # package = pkgs.nixUnstable;
+    # extraOptions = ''
+    #   auto-optimise-store = true
+    #   experimental-features = nix-command flakes repl-flake
+    #   warn-dirty = false
+    # '';
+    # gc = {
+    #   automatic = true;
+    #   dates = "weekly";
+    #   options = "--delete-older-than 30d";
+    # };
 
-    # Add each flake input as a registry
-    # To make nix3 commands consistent with the flake
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+    # # Add each flake input as a registry
+    # # To make nix3 commands consistent with the flake
+    # registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
 
-    # Map registries to channels
-    # Very useful when using legacy commands
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+    # # Map registries to channels
+    # # Very useful when using legacy commands
+    # nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
   };
 }
