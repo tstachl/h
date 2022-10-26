1075610f8f8efd4eea9c26af8977632b86cc1e55
Thomas Stachl <i@t5.st>
2022-10-26 11:08:15 -0700

    +state

--- hosts/throwaway/hardware-configuration.nix
+++ hosts/throwaway/hardware-configuration.nix
@@ -4,7 +4,7 @@
     (modulesPath + "/profiles/qemu-guest.nix")
     ../common/optional/filesystem.nix
     ../common/optional/fuse.nix
-    # ../common/optional/state.nix
+    ../common/optional/state.nix
     ../common/optional/systemd-boot.nix
   ];
 
