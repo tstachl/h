57e8b5f4490951799c41382bb7717a01fbee1f5f
Thomas Stachl <i@t5.st>
2022-11-01 12:42:03 -0700

    state is host dependent

--- hosts/thor/default.nix
+++ hosts/thor/default.nix
@@ -1,15 +1,55 @@
+{ lib, config, inputs, ... }:
 {
   imports = [
+    inputs.impermanence.nixosModules.impermanence
+
     ./hardware-configuration.nix
 
     ../common/global
-    ../common/optional/state.nix
     ../common/optional/systemd-boot.nix
     ../common/users/thomas.nix
 
     ../common/optional/nixos.nix
-    # ../common/optional/tailscale.nix
+    ../common/optional/tailscale.nix
   ];
+
+  environment.persistence."/persist" = {
+    hideMounts = true;
+
+    directories = [
+      "/var/log"
+      "/var/lib/systemd/coredump"
+    ];
+
+    files = [
+      "/etc/machine-id"
+    ];
+
+    users.thomas = {
+      directories = [
+        "Workspace"
+      ];
+
+      files = [
+        ".ssh/known_hosts"
+        ".config/gh/hosts.yml"
+      ];
+    };
+  };
+
+  services.openssh = lib.mkIf config.services.openssh.enable {
+    hostKeys = [
+      {
+        bits = 4096;
+        path = "/persist/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+      }
+      {
+        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
+  };
 
   networking.hostId = "575e22bc";
   networking.hostName = "thor";
--- hosts/throwaway/default.nix
+++ hosts/throwaway/default.nix
@@ -1,5 +1,8 @@
+{ lib, config, inputs, ... }:
 {
   imports = [
+    inputs.impermanence.nixosModules.impermanence
+
     ./hardware-configuration.nix
 
     ../common/global
@@ -13,7 +16,50 @@
     ../common/optional/yubikey.nix
   ];
 
+  environment.persistence."/persist" = {
+    hideMounts = true;
+
+    directories = [
+      "/var/log"
+      "/var/lib/bluetooth"
+      "/var/lib/systemd/coredump"
+      "/etc/NetworkManager/system-connections"
+    ];
+
+    files = [
+      "/etc/machine-id"
+    ];
+
+    users.thomas = {
+      directories = [
+        "Workspace"
+        { directory = ".config/BraveSoftware/"; mode = "0700"; }
+      ];
+
+      files = [
+        ".ssh/known_hosts"
+        ".config/monitors.xml"
+        ".config/gh/hosts.yml"
+        ".cache/rbw/https%3A%2F%2Fwarden.vault.pilina.com:bw-cli@pilina.email.json"
+      ];
+    };
+  };
+
+  services.openssh = lib.mkIf config.services.openssh.enable {
+    hostKeys = [
+      {
+        bits = 4096;
+        path = "/persist/etc/ssh/ssh_host_rsa_key";
+        type = "rsa";
+      }
+      {
+        path = "/persist/etc/ssh/ssh_host_ed25519_key";
+        type = "ed25519";
+      }
+    ];
+  };
+
   networking.hostName = "throwaway";
   time.timeZone = "America/Los_Angeles";
-  system.stateVersion = "22.05";
+  system.stateVersion = "22.11";
 }
