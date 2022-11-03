ba5c87fc8a806b1676169beb7d123c2f3791d161
Thomas Stachl <i@t5.st>
2022-11-03 14:52:50 -0300

    enable persistence for tailscale

--- hosts/common/optional/tailscale.nix
+++ hosts/common/optional/tailscale.nix
@@ -1,8 +1,24 @@
+{ lib, pkgs, ... }:
+
+with lib;
+
+let
+  inherit (pkgs.stdenv.hostPlatform) isLinux;
+in
 {
   services.tailscale.enable = true;
   networking.firewall = {
     checkReversePath = "loose";
   };
 
-  # TODO: find a way to auto-logi dn
+  boot.kernel = mkIf isLinux {
+    sysctl."net.ipv4.ip_forward" = 1;
+    sysctl."net.ipv6.conf.all.forwarding" = 1;
+  };
+
+  environment.persistence."/persist" = {
+    directories = [
+      "/var/lib/tailscale"
+    ];
+  };
 }
