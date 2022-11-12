ddca78609b2cd40bfe262e495b524f91d826faf2
Thomas Stachl <i@t5.st>
2022-11-12 09:04:48 -0800

    very conservative

--- hosts/penguin/hardware-configuration.nix
+++ hosts/penguin/hardware-configuration.nix
@@ -4,8 +4,8 @@ let
 in
 {
   imports = [
-    inputs.hardware.nixosModules.common-cpu-intel
-    inputs.hardware.nixosModules.common-gpu-intel
+    # inputs.hardware.nixosModules.common-cpu-intel
+    # inputs.hardware.nixosModules.common-gpu-intel
     # inputs.hardware.nixosModules.common-pc-laptop
 
     ../common/optional/systemd-boot.nix
@@ -50,9 +50,9 @@ in
   };
 
   nixpkgs.hostPlatform.system = "x86_64-linux";
-  networking.useDHCP = lib.mkDefault true;
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.video.hidpi.enable = lib.mkDefault true;
-  hardware.opengl.enable = lib.mkDefault true;
+  networking.useDHCP = true;
+  powerManagement.cpuFreqGovernor = "powersave";
+  # hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
+  # hardware.video.hidpi.enable = true;
+  # hardware.opengl.enable = true;
 }
