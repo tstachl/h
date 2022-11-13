a03e0346acde99cd81eccb5193283eb3f399e567
Thomas Stachl <i@t5.st>
2022-11-13 13:10:20 -0300

    create/jump to dir & clone repo if needed

--- home/thomas/features/cli/fish.nix
+++ home/thomas/features/cli/fish.nix
@@ -11,6 +11,27 @@
     };
 
     interactiveShellInit = lib.mkAfter ''
+      function repos -d "Jump to the Workspace directory"
+        set loc ~/Workspace
+        set target $loc/$argv[1]
+
+        if not test -d $target
+          set repo (string split "/" $argv[1])
+
+          if not test -d $loc/$repo[1]
+            echo "creating directory ..."
+            mkdir $loc/$repo[1]
+          end
+
+          cd $loc/$repo[1]
+          git clone git@github.com:$argv[1]
+        end
+
+        if test -d $target
+          cd $target
+        end
+      end
+
       function d -d "Develop on the given repository"
         set loc ~/Workspace
         set target $loc/$argv[1]
