0723be80c7d07fdc427393b148afcf597aca5363
Thomas Stachl <i@t5.st>
2022-11-13 10:59:44 -0800

    add encrypted keys for penguin and vault + rm throwaway from secrets

--- keys/secrets.nix
+++ keys/secrets.nix
@@ -3,8 +3,9 @@ let
   users = [ thomas ];
 
   thor = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINpKTrj5XDApmWLo2PpzBS3g8yn4G2CZlZUK/qgJFD2D";
-  throwaway = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPl8524nt4nERZLdrJg29pEUZlW1mnxgG+joc3qapHWc";
-  systems = [ thor throwaway ];
+  penguin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG0MBjL4rlCI1e8M6wjg8PUEcRwk3onjspLSj6okCA8h";
+  vault = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHBZgMEAnSsMpS9zRVZT8PYuZTZr9KX6lBKW93Fz1w9";
+  systems = [ thor penguin vault ];
 in
 {
   "cloudflare-token.age".publicKeys = [ thomas thor ];
