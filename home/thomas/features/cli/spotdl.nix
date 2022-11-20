6db87e76fb2ff8954ccc3baeaf0c93201620c10e
Thomas Stachl <i@t5.st>
2022-11-20 13:05:32 -0300

    change spotdl to a function using the current directory

--- home/thomas/features/cli/spotdl.nix
+++ home/thomas/features/cli/spotdl.nix
@@ -1,14 +1,16 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
-  # TODO: need to alias podman version instead.
-  programs.fish.shellAliases = {
-    spotdl = ''
-      ${pkgs.podman}/bin/podman run --rm \
-        -v ~/Music:/music \
-        spotdl/spotify-downloader \
-        --save-file=".spotdl" \
-        --format="flac" \
-        --output="{artist}/{album} ({year})/{track-number} - {title}" \
+  programs.fish = {
+    # TODO: check if certain named arguments are set and default if not
+    interactiveShellInit = lib.mkAfter ''
+      function spotdl -d "Run spotdl in the current directory with my defaults"
+        ${pkgs.podman}/bin/podman run --rm \
+          -v $(pwd):/music \
+          spotdl/spotify-downloader \
+          --save-file=".spotdl" \
+          --format="flac" \
+          --output="{artist}/{album} ({year})/{track-number} - {title}" \
+      end
     '';
   };
 }
