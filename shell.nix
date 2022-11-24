a818e16e3f1a40ef52420a00e8956b79086cfe42
Thomas Stachl <286093+tstachl@users.noreply.github.com>
2022-11-24 10:33:49 -0800

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -17,24 +17,24 @@
       # "nixpkgs=${inputs.nixpkgs}"
       # "nixpkgs-unstable=${inputs.unstable}"
   shellHook = ''
-    # gpg --list-keys i@t5.st || gpg --receive-keys DE749C31D060A160
-    # echo "$( \
-    #   gpg --list-keys \
-    #   | grep DE749C31D060A160 -A 1 \
-    #   | head -1 | tr -d '[:space:]' \
-    # ):6:" | gpg --import-ownertrust;
+    gpg --list-keys i@t5.st || gpg --receive-keys DE749C31D060A160
+    echo "$( \
+      gpg --list-keys \
+      | grep DE749C31D060A160 -A 1 \
+      | head -1 | tr -d '[:space:]' \
+    ):6:" | gpg --import-ownertrust;
 
-    # echo "pinentry-program $(which pinentry)" > ~/.gnupg/gpg-agent.conf
+    echo "pinentry-program $(which pinentry)" > ~/.gnupg/gpg-agent.conf
 
-    # export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-    # export PINENTRY_USER_DATA="USE_CURSES=1"
-    # export GPG_TTY=$(tty)
+    export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+    export PINENTRY_USER_DATA="USE_CURSES=1"
+    export GPG_TTY=$(tty)
 
-    # gpg-connect-agent reloadagent /bye
-    # gpg-connect-agent updatestartuptty /bye
+    gpg-connect-agent reloadagent /bye
+    gpg-connect-agent updatestartuptty /bye
   '';
 
-  nativeBuildInputs = with pkgs; [ nix ];
+  nativeBuildInputs = with pkgs; [ nix gnupg pinentry git ];
 }
 
 #   # nativeBuildInputs = with pkgs; [
