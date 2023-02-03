d43134de97f763454eab88a0e64ecb17c04fc8e3
Thomas Stachl <i@t5.st>
2023-02-03 14:12:07 -0800

    make escape the super key

--- hosts/common/darwin/desktop.nix
+++ hosts/common/darwin/desktop.nix
@@ -56,71 +56,71 @@
     enable = true;
     skhdConfig = ''
       # focus window
-      alt - h : yabai -m window --focus west
-      alt - j : yabai -m window --focus south
-      alt - k : yabai -m window --focus north
-      alt - l : yabai -m window --focus east
+      escape - h : yabai -m window --focus west
+      escape - j : yabai -m window --focus south
+      escape - k : yabai -m window --focus north
+      escape - l : yabai -m window --focus east
 
       # swap managed window
-      shift + alt - h : yabai -m window --swap west
-      shift + alt - j : yabai -m window --swap south
-      shift + alt - k : yabai -m window --swap north
-      shift + alt - l : yabai -m window --swap east
+      shift + escape - h : yabai -m window --swap west
+      shift + escape - j : yabai -m window --swap south
+      shift + escape - k : yabai -m window --swap north
+      shift + escape - l : yabai -m window --swap east
 
       # move managed window
-      shift + alt + ctrl - h : yabai -m window --warp west
-      shift + alt + ctrl - j : yabai -m window --warp south
-      shift + alt + ctrl - k : yabai -m window --warp north
-      shift + alt + ctrl - l : yabai -m window --warp east
+      shift + escape + ctrl - h : yabai -m window --warp west
+      shift + escape + ctrl - j : yabai -m window --warp south
+      shift + escape + ctrl - k : yabai -m window --warp north
+      shift + escape + ctrl - l : yabai -m window --warp east
 
       # rotate tree
-      alt - r : yabai -m space --rotate 90
+      escape - r : yabai -m space --rotate 90
 
       # toggle window fullscreen zoom
-      alt - f : yabai -m window --toggle zoom-fullscreen
+      escape - f : yabai -m window --toggle zoom-fullscreen
 
       # alt - s : yabai -m window --toggle
-      alt - s : yabai -m window --toggle sticky;\
+      escape - s : yabai -m window --toggle sticky;\
                 yabai -m window --toggle topmost;\
                 yabai -m window --toggle pip
 
       # toggle padding and gap
-      alt - g : yabai -m space --toggle padding; yabai -m space --toggle gap
+      escape - g : yabai -m space --toggle padding; yabai -m space --toggle gap
 
       # float / unfloat window and center on screen
-      alt - t : yabai -m window --toggle float;\
+      escape - t : yabai -m window --toggle float;\
                 yabai -m window --grid 4:4:1:1:2:2
 
       # toggle window split type
-      alt - e : yabai -m window --toggle split
+      escape - e : yabai -m window --toggle split
 
       # balance size of windows
-      shift + alt - 0 : yabai -m space --balance
+      shift + escape - 0 : yabai -m space --balance
 
       # move window and focus desktop
-      shift + alt - 1 : yabai -m window --space 1; yabai -m space --focus 1
-      shift + alt - 2 : yabai -m window --space 2; yabai -m space --focus 2
-      shift + alt - 3 : yabai -m window --space 3; yabai -m space --focus 3
-      shift + alt - 4 : yabai -m window --space 4; yabai -m space --focus 4
-      shift + alt - 5 : yabai -m window --space 5; yabai -m space --focus 5
-      shift + alt - 6 : yabai -m window --space 6; yabai -m space --focus 6
-      shift + alt - 7 : yabai -m window --space 7; yabai -m space --focus 7
-      shift + alt - 8 : yabai -m window --space 8; yabai -m space --focus 8
-      shift + alt - 9 : yabai -m window --space 9; yabai -m space --focus 9
+      shift + escape - 1 : yabai -m window --space 1; yabai -m space --focus 1
+      shift + escape - 2 : yabai -m window --space 2; yabai -m space --focus 2
+      shift + escape - 3 : yabai -m window --space 3; yabai -m space --focus 3
+      shift + escape - 4 : yabai -m window --space 4; yabai -m space --focus 4
+      shift + escape - 5 : yabai -m window --space 5; yabai -m space --focus 5
+      shift + escape - 6 : yabai -m window --space 6; yabai -m space --focus 6
+      shift + escape - 7 : yabai -m window --space 7; yabai -m space --focus 7
+      shift + escape - 8 : yabai -m window --space 8; yabai -m space --focus 8
+      shift + escape - 9 : yabai -m window --space 9; yabai -m space --focus 9
 
 
       # create desktop, move window and follow focus - uses jq for parsing json (brew install jq)
-      shift + alt - n : yabai -m space --create && \
+      shift + escape - n : yabai -m space --create && \
                          index="$(yabai -m query --spaces --display | jq 'map(select(."native-fullscreen" == 0))[-1].index')"&& \
                          yabai -m window --space "''${index}"&& \
                          yabai -m space --focus "''${index}"
 
       # fast focus desktop
-      alt - tab : yabai -m space --focus recent
+      escape - tab : yabai -m space --focus recent
 
       # send window to monitor and follow focus
-      shift + alt - n : yabai -m window --display next; yabai -m display --focus next
-      shift + alt - p : yabai -m window --display previous; yabai -m display --focus previous
+      shift + escape - n : yabai -m window --display next; yabai -m display --focus next
+      shift + escape - p : yabai -m window --display previous; yabai -m display --focus previous
     '';
   };
 
